be562c7507df5e582ec87394def0c7101fcdcfce
edmogeor <edmogeor@users.noreply.github.com>
2026-04-05 21:24:03 +0100

    chore: remove unnecessary test js scripts

--- test-admin-token.js
+++
@@ -1,7 +0,0 @@
-async function run() {
-  const strapi = (global).strapiInstance;
-  if (!strapi) return;
-  const adminService = strapi.service('admin::user');
-  console.log('Admin service exists?', !!adminService);
-}
-// Cannot run standalone if not in setup.ts context.
