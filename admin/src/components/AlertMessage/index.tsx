288a9928a52867e749037dd8f533592ccc41c2d7
edmogeor <edmogeor@users.noreply.github.com>
2026-04-06 13:12:38 +0100

    fix: resolve translation id showing on save modal

--- admin/src/components/AlertMessage/index.tsx
+++ admin/src/components/AlertMessage/index.tsx
@@ -1,62 +1,53 @@
-import {Alert} from "@strapi/design-system";
-import getTrad from "../../utils/getTrad";
-import {useIntl} from "react-intl";
-import styled from "styled-components";
+import { Alert } from '@strapi/design-system';
+import getTrad from '../../utils/getTrad';
+import { useIntl } from 'react-intl';
+import styled from 'styled-components';
 
 const AlertMessage = styled.div`
-    position: fixed;
-    left: 50%;
-    transform: translateX(-50%);
-    top: 2.875rem;
-    z-index: 10;
-    width: 31.25rem;
-`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 2.875rem;
+  z-index: 10;
+  width: 31.25rem;
+`;
 
-export function SuccessAlertMessage({onClose}: { onClose: () => void }) {
-  const {formatMessage} = useIntl();
+export function SuccessAlertMessage({ onClose }: { onClose: () => void }) {
+  const { formatMessage } = useIntl();
   return (
     <AlertMessage>
-      <Alert
-        title="Success"
-        variant={'success'}
-        closeLabel={''}
-        onClose={onClose}
-      >
+      <Alert title="Success" variant={'success'} closeLabel={''} onClose={onClose}>
         {formatMessage(getTrad('page.save.success'))}
       </Alert>
     </AlertMessage>
-  )
+  );
 }
 
-export function ErrorAlertMessage({onClose}: { onClose: () => void }) {
-  const {formatMessage} = useIntl();
+export function ErrorAlertMessage({ onClose }: { onClose: () => void }) {
+  const { formatMessage } = useIntl();
   return (
     <AlertMessage>
-      <Alert
-        title="Error"
-        variant={'danger'}
-        closeLabel={''}
-        onClose={onClose}
-      >
+      <Alert title="Error" variant={'danger'} closeLabel={''} onClose={onClose}>
         {formatMessage(getTrad('page.save.error'))}
       </Alert>
     </AlertMessage>
-  )
+  );
 }
 
-export function MatchedUserAlertMessage({onClose, count}: { onClose: () => void, count: number }) {
-  const {formatMessage} = useIntl();
-  const id = count > 1 ? 'tab.whitelist.users_exists' : 'tab.whitelist.user_exists';
+export function MatchedUserAlertMessage({
+  onClose,
+  count,
+}: {
+  onClose: () => void;
+  count: number;
+}) {
+  const { formatMessage } = useIntl();
+  const id = count > 1 ? 'whitelist.users_exists' : 'whitelist.user_exists';
   return (
     <AlertMessage>
-      <Alert
-        title="Info"
-        variant={'default'}
-        closeLabel={''}
-        onClose={onClose}
-      >
+      <Alert title="Info" variant={'default'} closeLabel={''} onClose={onClose}>
         {formatMessage(getTrad(id))}
       </Alert>
     </AlertMessage>
-  )
+  );
 }
